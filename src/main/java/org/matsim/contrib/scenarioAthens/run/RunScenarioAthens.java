029b50629e6064378483bf4540139affdfc8675f
panosgjuras <63541107+panosgjuras@users.noreply.github.com>
2024-11-04 10:30:35 +0200

    upload new network files, resilience

--- src/main/java/org/matsim/contrib/scenarioAthens/run/RunScenarioAthens.java
+++ src/main/java/org/matsim/contrib/scenarioAthens/run/RunScenarioAthens.java
@@ -74,8 +74,8 @@ public class RunScenarioAthens {
 			
 			fillConfigWithBicycleStandardValues(config);
 
-			config.network().setInputFile("sim4mtran_psafest_scenario_0_ATHENS.xml"); // Modify this
-			config.plans().setInputFile("attacked_network_1.xml");
+			config.network().setInputFile("attacked_network_1.xml"); // Modify this
+			config.plans().setInputFile("cropped_plans_4.xml");
 		
 		} else {
 			throw new RuntimeException("More than one argument was provided. There is no procedure for this situation. Thus aborting!"
